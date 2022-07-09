c5a96018555326f6c0f328f24825d2ad68c64829
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-09 22:38:03 +0900

    [#7] add Font local styleguide

--- Deartoday/Deartoday/Global/Extension/UIFont+.swift
+++ Deartoday/Deartoday/Global/Extension/UIFont+.swift
@@ -33,3 +33,49 @@ extension UIFont {
         return UIFont(name: "GmarketSans\(type.rawValue)", size: size)!
     }
 }
+
+// MARK: - Font StyleGuide
+
+extension UIFont {
+
+  class var h0: UIFont {
+    return UIFont(name: "GmarketSans-Medium", size: 24.0)!
+  }
+
+  class var p1: UIFont {
+    return UIFont(name: "Pretendard-Regular", size: 20.0)!
+  }
+
+  class var p0En: UIFont {
+    return UIFont(name: "GmarketSans-Medium", size: 19.0)!
+  }
+
+  class var btn1En: UIFont {
+    return UIFont(name: "GmarketSans-Bold", size: 16.0)!
+  }
+
+  class var btn0: UIFont {
+    return UIFont(name: "Pretendard-Medium", size: 16.0)!
+  }
+
+  class var p2: UIFont {
+    return UIFont(name: "Pretendard-Regular", size: 16.0)!
+  }
+
+  class var p3: UIFont {
+    return UIFont(name: "Pretendard-Medium", size: 15.0)!
+  }
+
+  class var caption0: UIFont {
+    return UIFont(name: "Pretendard-Regular", size: 14.0)!
+  }
+
+  class var onboard0: UIFont {
+    return UIFont(name: "Pretendard-Regular", size: 13.0)!
+  }
+
+  class var caption1: UIFont {
+    return UIFont(name: "Pretendard-Regular", size: 12.0)!
+  }
+    
+}
