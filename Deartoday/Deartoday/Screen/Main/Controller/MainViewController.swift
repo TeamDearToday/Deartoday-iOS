97cc326d38ea703c3316a6a1417be5ada088f2b2
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-10 17:12:44 +0900

    [#10] scrollview layout

--- Deartoday/Deartoday/Screen/Main/Controller/MainViewController.swift
+++ Deartoday/Deartoday/Screen/Main/Controller/MainViewController.swift
@@ -7,12 +7,33 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
+final class MainViewController: UIViewController {
+    
+    // MARK: - Property
+    
+    // MARK: - UI Property
+    
+    @IBOutlet var backgroundImageViewWidthConstraintCollection: [NSLayoutConstraint]!
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
     }
 
-
+    // MARK: - @objc
+    
+    // MARK: - Custom Method
+    
+    private func setUI() {
+        setConstraint()
+    }
+    
+    private func setConstraint() {
+        backgroundImageViewWidthConstraintCollection.forEach {
+            $0.constant = UIScreen.main.bounds.width
+        }
+    }
 }
 
