718657594e0d62f8d402715c147b7f5b02b28b9b
cchanmi <gkfn980@hs.ac.kr>
2022-07-12 16:10:31 +0900

    [#9] Label, Button 스보로 구현

--- Deartoday/Deartoday/Screen/Onboarding/Controller/OnboardingViewController.swift
+++ Deartoday/Deartoday/Screen/Onboarding/Controller/OnboardingViewController.swift
@@ -6,29 +6,25 @@
 //
 
 import UIKit
-import Then
+
 import SnapKit
+import Then
 
 class OnboardingViewController: UIViewController {
     
     // MARK: - Property
     
     // MARK: - UI Property
+    
     @IBOutlet var firstLabelCollection: [UILabel]!
     @IBOutlet weak var secondLabel: UILabel!
-    
+    @IBOutlet weak var nextButton: UIButton!
 
-    private var nextButton = DDSButton().then {
-        $0.style = .present
-        $0.text = "다음"
-        $0.addTarget(self, action: #selector(nextButtonDidTap), for: .touchUpInside)
-    }
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setLayout()
         setFirstLabelUI()
         setSecondLabelUI()
         setFirstAlpha()
@@ -38,12 +34,12 @@ class OnboardingViewController: UIViewController {
     
     // MARK: - @objc
     
-    @objc func nextButtonDidTap() {
+    // MARK: - Custom Method
+    
+    @IBAction func nextButtonDidTap(_ sender: Any) {
         setFirstAlpha()
         setSecondAnimation()
     }
-    
-    // MARK: - Custom Method
     
     private func setFirstLabelUI() {
         firstLabelCollection.forEach {
@@ -55,23 +51,10 @@ class OnboardingViewController: UIViewController {
     }
     
     private func setSecondLabelUI() {
-        secondLabel.then {
-            $0.textColor = .white
-            $0.font = .p1
-            $0.font = .systemFont(ofSize: 13)
-            $0.textAlignment = .center
-        }
-    }
-    
-    private func setLayout() {
-        view.addSubviews([nextButton])
-        
-        nextButton.snp.makeConstraints {
-            $0.top.equalTo(firstLabelCollection[1].snp.bottom).offset(79)
-            $0.leading.trailing.equalToSuperview().inset(10)
-            $0.bottom.equalToSuperview().offset(-46)
-        
-        }
+        secondLabel.textColor = .white
+        secondLabel.font = .p1
+        secondLabel.font = .systemFont(ofSize: 13)
+        secondLabel.textAlignment = .center
     }
     
     private func setFirstAlpha() {
