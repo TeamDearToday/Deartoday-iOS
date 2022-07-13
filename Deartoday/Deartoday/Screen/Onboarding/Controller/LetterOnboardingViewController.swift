a8dd307688d1bf711efc4af63f3caeb804f74f4b
cchanmi <gkfn980@hs.ac.kr>
2022-07-14 02:55:23 +0900

    [#38] 편지 온보딩 화면 전환

---
+++ Deartoday/Deartoday/Screen/Onboarding/Controller/LetterOnboardingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LetterOnboardingViewController.swift
+//  Deartoday
+//
+//  Created by 황찬미 on 2022/07/14.
+//
+
+import UIKit
+
+class LetterOnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
