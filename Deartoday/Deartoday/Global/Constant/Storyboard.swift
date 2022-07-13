43313e5e1aa073da5728182e6c1a1e7c53d73cbe
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-13 17:13:53 +0900

    [#7] ViewController, Storyboard명 상수화

---
+++ Deartoday/Deartoday/Global/Constant/Storyboard.swift
@@ -0,0 +1,25 @@
+//
+//  Storyboard.swift
+//  Deartoday
+//
+//  Created by 이경민 on 2022/07/13.
+//
+
+import Foundation
+
+extension Constant {
+    struct Storyboard {
+        ///온보딩
+        static let Onboarding = "Onboarding"
+        
+        ///메인
+        static let Main = "Main"
+        
+        ///메시지 확인 + 상세 확인
+        static let CheckMessage = "CheckMessage"
+        static let CheckMessageDetail = "CheckMessageDetail"
+        
+        ///시간여행 확인
+        static let CheckTimeTravel = "CheckTimeTravel"
+    }
+}
---
+++ Deartoday/Deartoday/Global/Constant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Deartoday
+//
+//  Created by 이경민 on 2022/07/13.
+//
+
+import Foundation
+
+extension Constant {
+    struct ViewController {
+        ///온보딩
+        static let Onboarding = "OnboardingViewController"
+        
+        ///메인
+        static let Main = "MainViewController"
+        
+        ///메시지 확인 + 상세 확인
+        static let CheckMessage = "CheckMessageViewController"
+        static let CheckMessageDetail = "CheckMessageDetailViewController"
+        
+        ///시간여행 확인
+        static let CheckTimeTravel = "CheckTimeTravelViewController"
+    }
+}
