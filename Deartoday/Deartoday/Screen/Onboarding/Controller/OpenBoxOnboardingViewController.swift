49104ae2f8276d0ffff4e955311c23a83e7e5e5f
cchanmi <gkfn980@hs.ac.kr>
2022-07-14 00:58:09 +0900

    [#38] OpenBox 온보딩 애니메이션 구현

---
+++ Deartoday/Deartoday/Screen/Onboarding/Controller/OpenBoxOnboardingViewController.swift
@@ -0,0 +1,66 @@
+//
+//  OpenBoxOnboardingViewController.swift
+//  Deartoday
+//
+//  Created by 황찬미 on 2022/07/13.
+//
+
+import UIKit
+
+class OpenBoxOnboardingViewController: UIViewController {
+
+    // MARK: - UI Property
+    @IBOutlet var labelCollection: [UILabel]!
+    @IBOutlet weak var letterButton: UIButton!
+    
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+    
+    // MARK: - Component UI Setting functions
+    
+    private func setUI() {
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.setComponentsAnimation()
+        }
+        
+        labelCollection.forEach {
+            $0.textColor = .white
+            $0.font = .p1
+            $0.font = .systemFont(ofSize: 13)
+        }
+        
+        labelCollection.forEach {
+            $0.isHidden = false
+        }
+        
+        letterButton.isHidden = false
+    }
+    
+    // MARK: - Animation function
+    
+    private func setComponentsAnimation() {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.labelCollection[0].transform = CGAffineTransform(translationX: 0, y: -16)
+            self.labelCollection[0].alpha = 1
+        }, completion: { _ in
+            UIView.animate(withDuration: 0.5, delay: 0.5, animations: {
+                self.labelCollection[1].transform = CGAffineTransform(translationX: 0, y: -16)
+                self.labelCollection[1].alpha = 1
+            }, completion: { _ in
+                UIView.animate(withDuration: 0.5, delay: 0.5, animations: {
+                    self.labelCollection[2].transform = CGAffineTransform(translationX: 0, y: -16)
+                    self.labelCollection[2].alpha = 1
+                }, completion: { _ in
+                    UIView.animate(withDuration: 0.3, delay: 0.3, animations: {
+                        self.letterButton.alpha = 1
+                    }, completion: nil )
+                })
+            })
+        })
+    }
+}
