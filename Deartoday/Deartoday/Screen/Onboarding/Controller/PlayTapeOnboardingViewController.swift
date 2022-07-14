c702a9226bde3f83df862b442c103c0c77d639af
cchanmi <gkfn980@hs.ac.kr>
2022-07-15 01:50:10 +0900

    [#49] 설명 label 추가

--- Deartoday/Deartoday/Screen/Onboarding/Controller/PlayTapeOnboardingViewController.swift
+++ Deartoday/Deartoday/Screen/Onboarding/Controller/PlayTapeOnboardingViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class PlayTapeOnboardingViewController: UIViewController {
-
+    
+    // MARK: - UI Property
+    
+    @IBOutlet weak var explanationLabel: UILabel!
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    // MARK: - @objc
+    
+    // MARK: - Custom Method
 
 }
