bae206ce4efa86e8fb689bac0f2bf92f0e393f6c
cchanmi <gkfn980@hs.ac.kr>
2022-07-15 04:25:36 +0900

    [#49] Components 생성

--- Deartoday/Deartoday/Screen/Onboarding/Controller/PlayTapeOnboardingViewController.swift
+++ Deartoday/Deartoday/Screen/Onboarding/Controller/PlayTapeOnboardingViewController.swift
@@ -7,21 +7,35 @@
 
 import UIKit
 
-class PlayTapeOnboardingViewController: UIViewController {
+final class PlayTapeOnboardingViewController: UIViewController {
     
     // MARK: - UI Property
     
     @IBOutlet weak var explanationLabel: UILabel!
+    @IBOutlet weak var circleButton: UIImageView!
+    @IBOutlet weak var tapeButton: UIButton!
+    @IBOutlet var circleCollection: [UIView]!
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setComponentsUI()
     }
     
-    // MARK: - @objc
-    
     // MARK: - Custom Method
-
+    
+    private func setComponentsUI() {
+        circleCollection.forEach {
+            $0.layer.cornerRadius = $0.frame.width / 2
+        }
+        explanationLabel.font = .onboard0
+        explanationLabel.textColor = .white
+    }
+    
+    // MARK: IBAction
+    
+    @IBAction func tapeButtonDidTap(_ sender: UIButton) {
+        guard let LetterOnboarding = UIStoryboard(name: Constant.Storyboard.Onboarding, bundle: nil).instantiateViewController(withIdentifier: Constant.ViewController.LetterOnboarding) as?
+    }
 }
