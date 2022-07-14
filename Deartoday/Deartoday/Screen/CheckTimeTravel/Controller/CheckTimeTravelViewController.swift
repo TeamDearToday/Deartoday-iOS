f252788a7b4e1754ac23e600690c732625f80c68
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-14 23:08:18 +0900

    [#26] 시간여행 확인 뷰 구현

--- Deartoday/Deartoday/Screen/CheckTimeTravel/Controller/CheckTimeTravelViewController.swift
+++ Deartoday/Deartoday/Screen/CheckTimeTravel/Controller/CheckTimeTravelViewController.swift
@@ -7,12 +7,24 @@
 
 import UIKit
 
+enum Section {
+    case tape
+}
+
 final class CheckTimeTravelViewController: UIViewController {
+    
+    // MARK: - Property
+    
+    var dataSource: UITableViewDiffableDataSource<Section, TimeTapeDataModel>!
+    var snapshot: NSDiffableDataSourceSnapshot<Section, TimeTapeDataModel>!
+    var timeTapes: [TimeTapeDataModel] = []
 
     // MARK: - UI Property
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var emptyView: UIView!
     @IBOutlet weak var emptyDescriptionLabel: UILabel!
     @IBOutlet weak var emptyTitleLabel: UILabel!
     @IBOutlet weak var rewindImageView: UIImageView!
@@ -22,6 +34,7 @@ final class CheckTimeTravelViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
+        setTableView()
     }
     
     // MARK: - Custom Method
@@ -29,6 +42,34 @@ final class CheckTimeTravelViewController: UIViewController {
     private func setUI() {
         setHeaderViewUI()
         setEmptyViewUI()
+    }
+    
+    private func setTableView() {
+        registerXib()
+        setTableViewLayout()
+        setDataSource()
+    }
+    
+    private func registerXib() {
+        let nib = UINib(nibName: TimeTapeTableViewCell.identifier, bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: TimeTapeTableViewCell.identifier)
+    }
+    
+    private func setTableViewLayout() {
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 273
+    }
+    
+    private func setDataSource() {
+        dataSource = UITableViewDiffableDataSource<Section, TimeTapeDataModel>(tableView: tableView, cellProvider: { (tableView: UITableView, indexPath: IndexPath, identifier: TimeTapeDataModel) -> UITableViewCell? in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: TimeTapeTableViewCell.identifier, for: indexPath) as? TimeTapeTableViewCell else { return UITableViewCell() }
+            cell.setData(model: self.timeTapes[indexPath.item])
+            return cell
+        })
+        snapshot = NSDiffableDataSourceSnapshot<Section, TimeTapeDataModel>()
+        snapshot.appendSections([.tape])
+        snapshot.appendItems(timeTapes, toSection: .tape)
+        dataSource.apply(snapshot, animatingDifferences: true)
     }
     
     // MARK: - IBAction
