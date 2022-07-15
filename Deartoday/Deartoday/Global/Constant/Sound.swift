bf5b6c28b856fa6fe7feed9e4966a6e7640de821
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-16 04:18:17 +0900

    [#] add box sound const

---
+++ Deartoday/Deartoday/Global/Constant/Sound.swift
@@ -0,0 +1,15 @@
+//
+//  Sound.swift
+//  Deartoday
+//
+//  Created by 이경민 on 2022/07/16.
+//
+
+import Foundation
+
+extension Constant {
+    struct Sound {
+        //box
+        static let sound_box = "sound_box"
+    }
+}
