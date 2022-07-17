f400d91f01d086fad54c54fff8ab5a743970f06a
cchanmi <gkfn980@hs.ac.kr>
2022-07-18 01:22:59 +0900

    [#67] 소셜로그인 UI 구현

--- Deartoday/Deartoday/Screen/Initial/Controller/InitialViewContoller.swift
+++ Deartoday/Deartoday/Screen/Initial/Controller/InitialViewContoller.swift
@@ -5,4 +5,52 @@
 //  Created by 황찬미 on 2022/07/18.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class InitialViewController: UIViewController {
+    // MARK: - Property
+    
+    // MARK: - UI Property
+    
+    var backgroundView = UIView().then {
+        $0.backgroundColor = .lightBlue00
+    }
+    
+    var logoImageView = UIImageView().then {
+        $0.image = Constant.Image.logo
+    }
+    
+    var appleLoginButton = UIButton().then {
+        $0.setBackgroundImage(Constant.Image.btnApple, for: .normal)
+    }
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setLayout()
+    }
+    
+    // MARK: - @objc
+    
+    // MARK: - Custom Method
+    
+    private func setLayout() {
+        view.addSubviews([backgroundView, logoImageView, appleLoginButton])
+        
+        backgroundView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        logoImageView.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(68)
+            $0.top.equalToSuperview().inset(258)
+        }
+        appleLoginButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.top.equalTo(logoImageView.snp.bottom).inset(10)
+        }
+    }
+}
