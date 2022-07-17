534b0bf967ed33f1a4002128ab360202586ee874
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-18 00:47:04 +0900

    [#68] add Main navigation controller file

---
+++ Deartoday/Deartoday/Screen/Main/Controller/MainNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  MainNavigationController.swift
+//  Deartoday
+//
+//  Created by 이경민 on 2022/07/18.
+//
+
+import UIKit
+
+final class MainNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
