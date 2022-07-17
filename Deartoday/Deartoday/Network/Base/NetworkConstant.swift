6924d6df5f0b28b21a16f65657f98ffc6780477d
so yeon <alicesysy@swu.ac.kr>
2022-07-18 01:39:28 +0900

    [#66] 시간여행 가상공간 이미지 GET 서버통신

--- Deartoday/Deartoday/Network/Base/NetworkConstant.swift
+++ Deartoday/Deartoday/Network/Base/NetworkConstant.swift
@@ -11,7 +11,7 @@ struct NetworkConstant {
     
     static let noTokenHeader = ["Content-Type": "application/json"]
     static let hasTokenHeader = ["Content-Type": "application/json",
-                                 "accessToken": NetworkConstant.accessToken]
+                                 "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJkM2FmYjZhOTc0MWNmMjFhOTg5ZTk0In0sImlhdCI6MTY1ODA3MjM2NywiZXhwIjoxNjU4MDc1OTY3fQ.EOSvL0yAlCnyMv4rBIGvWWPCDfFnu9Pw-1w-fkX8ITg"]
     
-    static var accessToken = ""
+    static var accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJkM2FmYjZhOTc0MWNmMjFhOTg5ZTk0In0sImlhdCI6MTY1ODA3MjM2NywiZXhwIjoxNjU4MDc1OTY3fQ.EOSvL0yAlCnyMv4rBIGvWWPCDfFnu9Pw-1w-fkX8ITg"
 }
