08bbf78ba6848a2aef02b0c9a2f3bf6a20ca2000
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-18 02:21:48 +0900

    [#72] Main Data Model 추가

---
+++ Deartoday/Deartoday/Network/DataModel/Main/MainDataModel.swift
@@ -0,0 +1,14 @@
+//
+//  MainDataModel.swift
+//  Deartoday
+//
+//  Created by 이경민 on 2022/07/18.
+//
+
+import Foundation
+
+// MARK: - MainDataModel
+
+struct MainDataModel: Codable {
+    let timeTravelCount: Int
+}
