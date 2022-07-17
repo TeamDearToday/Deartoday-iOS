388ee4998f443da920332d60f4baf972338fc045
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-18 03:41:05 +0900

    [#75] add check time travel service

--- Deartoday/Deartoday/Network/Service/CheckTimeTravel/CheckTimeTravelService.swift
+++ Deartoday/Deartoday/Network/Service/CheckTimeTravel/CheckTimeTravelService.swift
@@ -5,4 +5,34 @@
 //  Created by 이경민 on 2022/07/18.
 //
 
-import Foundation
+import Moya
+
+enum CheckTimeTravel {
+    case checkTimeTravel
+}
+
+extension CheckTimeTravel: BaseTargetType {
+    var path: String {
+        switch self {
+        case .checkTimeTravel:
+            return URLConstant.timeTravel
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        default: return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .checkTimeTravel:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        return NetworkConstant.hasTokenHeader
+    }
+}
