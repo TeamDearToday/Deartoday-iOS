f7cc9bdcaad9d51ec29eee1b468351401de85170
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-18 21:58:26 +0900

    [#80] token 변경

--- Deartoday/Deartoday/Network/Base/NetworkConstant.swift
+++ Deartoday/Deartoday/Network/Base/NetworkConstant.swift
@@ -11,7 +11,7 @@ struct NetworkConstant {
     
     static let noTokenHeader = ["Content-Type": "application/json"]
     static let hasTokenHeader = ["Content-Type": "application/json",
-                                 "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJkM2FmYjZhOTc0MWNmMjFhOTg5ZTk0In0sImlhdCI6MTY1ODA3OTQ0MSwiZXhwIjoxNjU4MDgzMDQxfQ.yfcH2b_FO3gMI8hML14l8mijP1WHCkeanyrUUYIbaVQ"]
+                                 "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJjZTgwZDk2ZDZlMjJlOGMzNjFhNDVkIn0sImlhdCI6MTY1ODE0ODY5MywiZXhwIjoxNjU4MTUyMjkzfQ.g7i4SLpLy2dffMRiVUR7PsjmDioNXNtc0HqlugyV8ts"]
     
     static var accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJkM2FmYjZhOTc0MWNmMjFhOTg5ZTk0In0sImlhdCI6MTY1ODA3MjM2NywiZXhwIjoxNjU4MDc1OTY3fQ.EOSvL0yAlCnyMv4rBIGvWWPCDfFnu9Pw-1w-fkX8ITg"
 }
