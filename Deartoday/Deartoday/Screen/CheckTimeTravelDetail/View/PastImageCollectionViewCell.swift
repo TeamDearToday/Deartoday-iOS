3d53717186c3e63b9d6bab2bc6de54f6250f5997
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-18 15:51:28 +0900

    [#61] 시간 여행 상세 사진 셀 파일 추가

---
+++ Deartoday/Deartoday/Screen/CheckTimeTravelDetail/View/PastImageCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PastImageCollectionViewCell.swift
+//  Deartoday
+//
+//  Created by 이경민 on 2022/07/18.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class PastImageCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Property
+    // MARK: - UI Property
+    // MARK: - Life Cycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    // MARK: - @objc
+    // MARK: - Custom Method
+    // MARK: - IBAction
+}
