648d689fe833cd0aabb89f7728e8198b78bd03ce
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-18 22:55:56 +0900

    [#79] impl check message service

--- Deartoday/Deartoday/Network/Service/CheckMessage/CheckMessageService.swift
+++ Deartoday/Deartoday/Network/Service/CheckMessage/CheckMessageService.swift
@@ -5,4 +5,34 @@
 //  Created by 이경민 on 2022/07/18.
 //
 
-import Foundation
+import Moya
+
+enum CheckMessageService {
+    case checkMessage
+}
+
+extension CheckMessageService: BaseTargetType {
+    var path: String {
+        switch self {
+        case .checkMessage:
+            return URLConstant.answers
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        default: return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .checkMessage:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        return NetworkConstant.hasTokenHeader
+    }
+}
