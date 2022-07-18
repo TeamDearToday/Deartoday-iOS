e77163a8f220af7d7119e462a9e567fe2c3034f3
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-18 23:30:35 +0900

    [#79] impl check message api

--- Deartoday/Deartoday/Network/API/CheckMessage/CheckMessageAPI.swift
+++ Deartoday/Deartoday/Network/API/CheckMessage/CheckMessageAPI.swift
@@ -5,4 +5,32 @@
 //  Created by 이경민 on 2022/07/18.
 //
 
-import Foundation
+import Moya
+
+final class CheckMessageAPI {
+    static let shared: CheckMessageAPI = CheckMessageAPI()
+    private let checkMessageProvider = MoyaProvider<CheckMessageService>(plugins: [MoyaLoggingPlugin()])
+    private init() { }
+    
+    public private(set) var checkMessageData: GeneralResponse<[String]>?
+    
+    // MARK: - GET
+    
+    func getCheckMessage(completion: @escaping (GeneralResponse<[String]>?) -> ()) {
+        checkMessageProvider.request(.checkMessage) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    self.checkMessageData = try response.map(GeneralResponse<[String]>?.self)
+                    guard let checkMessageData = self.checkMessageData else { return }
+                    completion(checkMessageData)
+                } catch(let err) {
+                    print(err.localizedDescription, 500)
+                }
+            case .failure(let err):
+                print(err.localizedDescription)
+                completion(nil)
+            }
+        }
+    }
+}
