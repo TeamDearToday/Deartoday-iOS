536daaf6a3b49d37106e19dd4ebe0c2eb0b8ad20
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-18 16:00:22 +0900

    [#61] section view 파일 추가

---
+++ Deartoday/Deartoday/Screen/CheckTimeTravelDetail/View/TimeTravelInfoView.swift
@@ -0,0 +1,27 @@
+//
+//  TimeTravelInfoView.swift
+//  Deartoday
+//
+//  Created by 이경민 on 2022/07/18.
+//
+
+import UIKit
+
+final class TimeTravelInfoView: UIView {
+
+    // MARK: - Property
+    // MARK: - UI Property
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - @objc
+    // MARK: - Custom Method
+    // MARK: - IBAction
+}
