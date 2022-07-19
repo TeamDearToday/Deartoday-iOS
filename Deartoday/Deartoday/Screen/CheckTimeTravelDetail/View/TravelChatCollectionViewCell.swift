7e17dfcb2b89865e17358ac632a4506157dd2385
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-20 01:37:46 +0900

    [#61] chat collection view cell 구현

--- Deartoday/Deartoday/Screen/CheckTimeTravelDetail/View/TravelChatCollectionViewCell.swift
+++ Deartoday/Deartoday/Screen/CheckTimeTravelDetail/View/TravelChatCollectionViewCell.swift
@@ -7,18 +7,40 @@
 
 import UIKit
 
-class TravelChatCollectionViewCell: UICollectionViewCell {
+final class TravelChatCollectionViewCell: UICollectionViewCell {
 
     static let identifier = "TravelChatCollectionViewCell"
     
     // MARK: - Property
     // MARK: - UI Property
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var questionImageViewWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var questionImageViewHeightConstraint: UIImageView!
+    @IBOutlet weak var answerImageViewWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var answerImageViewHeightConstraint: NSLayoutConstraint!
+    
     // MARK: - Life Cycle
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        setUI()
     }
 
     // MARK: - Custom Method
+    
+    private func setUI() {
+        questionLabel.font = .caption2
+        answerLabel.font = .caption2
+    }
+    
+    private func updateUI() {
+    }
 
+    func setData(question: String, answer: String) {
+        questionLabel.text = question
+        answerLabel.text = answer
+        updateUI()
+    }
 }
