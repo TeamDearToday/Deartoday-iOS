0c1cc83f6cb1cce6658dd1772fc6f3481271ccad
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-19 17:07:45 +0900

    [#61] add TravelLastCollectionViewCell

---
+++ Deartoday/Deartoday/Screen/CheckTimeTravelDetail/View/TravelLastCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TravelLastCollectionViewCell.swift
+//  Deartoday
+//
+//  Created by 이경민 on 2022/07/19.
+//
+
+import UIKit
+
+class TravelLastCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
