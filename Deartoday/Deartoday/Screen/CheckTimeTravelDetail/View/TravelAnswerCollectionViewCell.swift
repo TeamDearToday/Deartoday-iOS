69829e204e51c8073cbc090ef95cbd5bfd1499cb
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-19 23:56:48 +0900

    [#61] detail TODO 추가

--- Deartoday/Deartoday/Screen/CheckTimeTravelDetail/View/TravelAnswerCollectionViewCell.swift
+++ Deartoday/Deartoday/Screen/CheckTimeTravelDetail/View/TravelAnswerCollectionViewCell.swift
@@ -34,7 +34,9 @@ final class TravelAnswerCollectionViewCell: UICollectionViewCell {
     }
     
     private func updateImageViewUI() {
-        
+        //image view height는 라벨의 현재 사이즈에 28 더하기
+        //width는 32 더하기
+        //label width 정해놓긴 했는데 안댈라나?
     }
     
     func setData(answer: String) {
