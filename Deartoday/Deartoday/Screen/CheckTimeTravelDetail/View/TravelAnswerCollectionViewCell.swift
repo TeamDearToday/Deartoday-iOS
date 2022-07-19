51bcf71ba8282edcc42628ac3ad4f943278010a5
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-19 17:07:17 +0900

    [#61] add TravelAnswerCollectionViewCell

---
+++ Deartoday/Deartoday/Screen/CheckTimeTravelDetail/View/TravelAnswerCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAnswerCollectionViewCell.swift
+//  Deartoday
+//
+//  Created by 이경민 on 2022/07/19.
+//
+
+import UIKit
+
+class TravelAnswerCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
