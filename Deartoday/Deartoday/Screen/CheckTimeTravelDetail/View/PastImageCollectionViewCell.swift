145bef199b9dc5441d9448e722d5e1e7e73cff51
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-19 17:06:09 +0900

    [#61] add image collection view cell file

---
+++ Deartoday/Deartoday/Screen/CheckTimeTravelDetail/View/PastImageCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PastImageCollectionViewCell.swift
+//  Deartoday
+//
+//  Created by 이경민 on 2022/07/19.
+//
+
+import UIKit
+
+class PastImageCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
