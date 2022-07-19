afee3c7bee5319c69989071f50397f240bd5465c
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-19 23:51:39 +0900

    [#61] impl last answer collection view cell

--- Deartoday/Deartoday/Screen/CheckTimeTravelDetail/View/TravelAnswerCollectionViewCell.swift
+++ Deartoday/Deartoday/Screen/CheckTimeTravelDetail/View/TravelAnswerCollectionViewCell.swift
@@ -11,9 +11,34 @@ final class TravelAnswerCollectionViewCell: UICollectionViewCell {
     
     static let identifier = "TravelAnswerCollectionViewCell"
 
+    // MARK: - UI Property
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var contentImageView: UIImageView!
+    @IBOutlet weak var contentImageViewWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var contentImageVIewHeightConstraint: NSLayoutConstraint!
+    
+    // MARK: - Life Cycle
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setUI()
     }
-
+    
+    // MARK: - Custom Method
+    
+    private func setUI() {
+        titleLabel.font = .caption2
+        contentLabel.font = .caption2
+    }
+    
+    private func updateImageViewUI() {
+        
+    }
+    
+    func setData(answer: String) {
+        contentLabel.text = answer
+        updateImageViewUI()
+    }
 }
