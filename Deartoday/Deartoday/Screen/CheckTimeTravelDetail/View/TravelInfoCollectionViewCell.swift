26121555ae9e990da8940f0a7f3241c0081808f5
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-19 22:22:35 +0900

    [#61] impl travel info cell

--- Deartoday/Deartoday/Screen/CheckTimeTravelDetail/View/TravelInfoCollectionViewCell.swift
+++ Deartoday/Deartoday/Screen/CheckTimeTravelDetail/View/TravelInfoCollectionViewCell.swift
@@ -11,9 +11,30 @@ final class TravelInfoCollectionViewCell: UICollectionViewCell {
     
     static let identifier = "TravelInfoCollectionViewCell"
 
+    // MARK: - UI Property
+    
+    @IBOutlet weak var travelDateLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var writtenDateLabel: UILabel!
+    
+    // MARK: - Life Cycle
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setUI()
     }
 
+    // MARK: - Custom Method
+    
+    private func setUI() {
+        travelDateLabel.font = .h0
+        titleLabel.font = .h2
+        writtenDateLabel.font = .caption0
+    }
+    
+    func setData(written: String, to: String, title: String) {
+        titleLabel.text = title
+        writtenDateLabel.text = written
+        travelDateLabel.text = to
+    }
 }
