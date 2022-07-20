66c2edc47fd84d81238b0760b7556e039054b7b3
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-21 00:04:18 +0900

    [#101] model 변경사항 반영

--- Deartoday/Deartoday/Network/API/CheckMessage/CheckMessageAPI.swift
+++ Deartoday/Deartoday/Network/API/CheckMessage/CheckMessageAPI.swift
@@ -12,16 +12,16 @@ final class CheckMessageAPI {
     private let checkMessageProvider = MoyaProvider<CheckMessageService>(plugins: [MoyaLoggingPlugin()])
     private init() { }
     
-    public private(set) var checkMessageData: GeneralResponse<[CheckMessageResponse]>?
+    public private(set) var checkMessageData: GeneralResponse<CheckMessageResponse>?
     
     // MARK: - GET
     
-    func getCheckMessage(completion: @escaping (GeneralResponse<[CheckMessageResponse]>?) -> ()) {
+    func getCheckMessage(completion: @escaping (GeneralResponse<CheckMessageResponse>?) -> ()) {
         checkMessageProvider.request(.checkMessage) { result in
             switch result {
             case .success(let response):
                 do {
-                    self.checkMessageData = try response.map(GeneralResponse<[CheckMessageResponse]>?.self)
+                    self.checkMessageData = try response.map(GeneralResponse<CheckMessageResponse>?.self)
                     guard let checkMessageData = self.checkMessageData else { return }
                     completion(checkMessageData)
                 } catch(let err) {
