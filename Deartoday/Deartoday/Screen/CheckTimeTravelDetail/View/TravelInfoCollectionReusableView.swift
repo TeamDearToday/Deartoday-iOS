d2536a331f9d405543974746e49ada8713b1d1d7
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-20 18:08:47 +0900

    [#61] Header reusableview 구현

---
+++ Deartoday/Deartoday/Screen/CheckTimeTravelDetail/View/TravelInfoCollectionReusableView.swift
@@ -0,0 +1,42 @@
+//
+//  TravelInfoCollectionReusableView.swift
+//  Deartoday
+//
+//  Created by 이경민 on 2022/07/20.
+//
+
+import UIKit
+
+final class TravelInfoCollectionReusableView: UICollectionReusableView {
+    
+    static let identifier = "TravelInfoCollectionReusableView"
+
+    // MARK: - Property
+    
+    // MARK: - UI Property
+    
+    @IBOutlet weak var pastDateLabel: UILabel!
+    @IBOutlet weak var writtenDateLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    // MARK: - Life Cycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUI()
+    }
+    
+    // MARK: - Custom Method
+    
+    private func setUI() {
+        pastDateLabel.font = .h0
+        writtenDateLabel.font = .caption0
+        titleLabel.font = .h2
+    }
+    
+    func setData(past: String, written: String, title: String) {
+        pastDateLabel.text = past
+        writtenDateLabel.text = written
+        titleLabel.text = title
+    }
+}
