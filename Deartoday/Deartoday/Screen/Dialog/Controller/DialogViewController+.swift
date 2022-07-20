f3d65c1a72df74766dc3d7ec8df22065b23da280
so yeon <alicesysy@swu.ac.kr>
2022-07-20 22:16:38 +0900

    [#102] 대화방 반응 추가 및 애니메이션 수정

--- Deartoday/Deartoday/Screen/Dialog/Controller/DialogViewController+.swift
+++ Deartoday/Deartoday/Screen/Dialog/Controller/DialogViewController+.swift
@@ -10,7 +10,7 @@ import UIKit
 extension DialogViewController {
     // 나레이션 라벨 애니메이션
     internal func showNarrationLabel(_ component: UILabel, completion: @escaping () -> Void) {
-        UIView.animate(withDuration: 0.5, delay: 0.7, options: .curveEaseOut) {
+        UIView.animate(withDuration: 0.5, delay: 0.5, options: .curveEaseOut) {
             component.transform = CGAffineTransform(translationX: 0, y: -16)
             component.alpha = 1
         } completion: { _ in
@@ -19,7 +19,7 @@ extension DialogViewController {
     }
     
     internal func hideNarrationLabel(_ component: UILabel, completion: @escaping () -> Void) {
-        UIView.animate(withDuration: 0.5, delay: 0.7, options: .curveEaseOut) {
+        UIView.animate(withDuration: 0.5, delay: 0.5, options: .curveEaseOut) {
             component.alpha = 0
         } completion: { _ in
             completion()
@@ -28,7 +28,7 @@ extension DialogViewController {
     
     // 버튼 애니메이션
     internal func showButton(_ component: UIButton, completion: @escaping () -> Void) {
-        UIView.animate(withDuration: 0.6, delay: 0.6, options: .curveEaseOut) {
+        UIView.animate(withDuration: 0.6, delay: 0.5, options: .curveEaseOut) {
             component.alpha = 1
         } completion: { _ in
             completion()
@@ -36,7 +36,7 @@ extension DialogViewController {
     }
     
     internal func hideButton(_ component: UIButton, completion: @escaping () -> Void) {
-        UIView.animate(withDuration: 0.6, delay: 0.6, options: .curveEaseOut) {
+        UIView.animate(withDuration: 0.5, delay: 0.5, options: .curveEaseOut) {
             component.alpha = 0
         } completion: { _ in
             completion()
@@ -45,14 +45,14 @@ extension DialogViewController {
     
     // 과거의 나 텍스트 애니메이션
     internal func showPastView(_ component: UIView, completion: @escaping () -> Void) {
-        UIView.animate(withDuration: 0.3, delay: 0.8, options: .curveEaseOut) {
+        UIView.animate(withDuration: 0.3, delay: 0.5, options: .curveEaseOut) {
             component.alpha = 1
         } completion: { _ in
             completion()
         }
     }
     
-    internal func hidePastView(_ component: UIView, delay: Double = 0.8, duration: Double = 0.3, completion: @escaping () -> Void) {
+    internal func hidePastView(_ component: UIView, delay: Double = 0.5, duration: Double = 0.5, completion: @escaping () -> Void) {
         UIView.animate(withDuration: duration, delay: delay, options: .curveEaseOut) {
             component.alpha = 0
         } completion: { _ in
@@ -78,13 +78,13 @@ extension DialogViewController {
     }
     
     // 나타나는 애니메이션
-    internal func showView(_ component: UIView, delay: Double = 0.8, duration: Double = 0.3){
+    internal func showView(_ component: UIView, delay: Double = 0.5, duration: Double = 0.3){
         UIView.animate(withDuration: duration, delay: delay, options: .curveEaseOut) {
             component.alpha = 1
         }
     }
     
-    internal func hideView(_ component: UIView, delay: Double = 0.8, duration: Double = 0.3){
+    internal func hideView(_ component: UIView, delay: Double = 0.5, duration: Double = 0.3){
         UIView.animate(withDuration: duration, delay: delay, options: .curveEaseOut) {
             component.alpha = 0
         } 
