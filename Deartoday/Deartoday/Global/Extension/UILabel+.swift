a01170148ab88501d913c6c0afea3d8a74a78857
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-21 17:39:45 +0900

    [#56] line height 지정하는 label extension 추가

--- Deartoday/Deartoday/Global/Extension/UILabel+.swift
+++ Deartoday/Deartoday/Global/Extension/UILabel+.swift
@@ -42,6 +42,21 @@ extension UILabel {
         }
         attributedText = attributedString
     }
+    
+    func setTextWithLineHeight(text: String?, lineHeight: CGFloat){
+       if let text = text {
+           let style = NSMutableParagraphStyle()
+           style.maximumLineHeight = lineHeight
+           style.minimumLineHeight = lineHeight
+
+           let attributes: [NSAttributedString.Key: Any] = [
+               .paragraphStyle: style,
+               .baselineOffset: (lineHeight - font.lineHeight) / 4
+           ]
+           
+           let attrString = NSAttributedString(string: text,
+                                               attributes: attributes)
+           self.attributedText = attrString
+       }
+   }
 }
-
-
