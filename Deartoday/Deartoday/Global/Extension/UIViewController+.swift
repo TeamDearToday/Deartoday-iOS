8b0cc0f55aeab589d6b7d9be163b5b34b4caaf5b
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-07-21 23:54:03 +0900

    [#121] add change root viewcontroller extension

--- Deartoday/Deartoday/Global/Extension/UIViewController+.swift
+++ Deartoday/Deartoday/Global/Extension/UIViewController+.swift
@@ -51,4 +51,14 @@ extension UIViewController {
     func getDeviceHeight() -> CGFloat {
         return UIScreen.main.bounds.height
     }
+    
+    func changeRootViewController(_ viewControllerToPresent: UIViewController) {
+        if let window = UIApplication.shared.windows.first {
+            window.rootViewController = viewControllerToPresent
+            UIView.transition(with: window, duration: 0.5, options: .transitionCrossDissolve, animations: nil)
+        } else {
+            viewControllerToPresent.modalPresentationStyle = .overFullScreen
+            self.present(viewControllerToPresent, animated: true, completion: nil)
+        }
+    }
 }
