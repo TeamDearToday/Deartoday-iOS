d8599f2437c8b6477884574fffb47cc5fc53834e
cchanmi <gkfn980@hs.ac.kr>
2022-08-04 23:09:37 +0900

    [Chore] 소셜로그인 버튼 커스텀

--- Deartoday/Deartoday/Screen/Initial/Controller/InitialViewContoller.swift
+++ Deartoday/Deartoday/Screen/Initial/Controller/InitialViewContoller.swift
@@ -25,8 +25,33 @@ final class InitialViewController: UIViewController {
     }
     
     var appleLoginButton = UIButton().then {
-        $0.setBackgroundImage(Constant.Image.btnApple, for: .normal)
+        $0.backgroundColor = .black
+        $0.makeRound(radius: 4)
         $0.addTarget(self, action: #selector(appleSignInButtonDidTap), for: .touchUpInside)
+    }
+    
+    var appleLogo = UIImageView().then {
+        $0.image = UIImage(systemName: "applelogo")
+        $0.contentMode = .scaleAspectFill
+        $0.tintColor = .white
+    }
+    
+    var appleLoginLabel = UILabel().then {
+        $0.text = "Apple로 계속하기"
+        $0.textColor = .white
+        $0.font = .p8
+        $0.sizeToFit()
+    }
+    
+    var appleLoginView = UIView().then {
+        $0.backgroundColor = .clear
+        $0.isUserInteractionEnabled = true
+    }
+    
+    var appleLoginStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 8
+        $0.alignment = .center
     }
     
     // MARK: - Life Cycle
@@ -34,6 +59,7 @@ final class InitialViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setLayout()
+        addAppleLoginViewGesture()
     }
     
     // MARK: - @objc
@@ -51,19 +77,52 @@ final class InitialViewController: UIViewController {
     
     // MARK: - Custom Method
     
+    private func presentMainView() {
+        guard let mainViewController = UIStoryboard(name: Constant.Storyboard.Main, bundle: nil).instantiateViewController(withIdentifier: "MainNavigationController") as? MainNavigationController else { return }
+        mainViewController.modalPresentationStyle = .fullScreen
+        mainViewController.modalTransitionStyle = .crossDissolve
+        present(mainViewController, animated: true)
+    }
+    
+    private func addAppleLoginViewGesture() {
+        let addAppleLoginViewGesture = UITapGestureRecognizer(target: self, action: #selector(appleSignInButtonDidTap))
+        appleLoginView.addGestureRecognizer(addAppleLoginViewGesture)
+    }
+    
     private func setLayout() {
-        view.addSubviews([backgroundView, logoImageView, appleLoginButton])
+        view.addSubviews([backgroundView,
+                          logoImageView,
+                          appleLoginButton,
+                          appleLoginStackView,
+                          appleLoginView])
+        appleLoginStackView.addArrangedSubview(appleLogo)
+        appleLoginStackView.addArrangedSubview(appleLoginLabel)
         
         backgroundView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
+        
         logoImageView.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.top.equalToSuperview().inset(getDeviceHeight() == 667 ? 190 : 258)
+            $0.centerY.equalToSuperview().multipliedBy(0.9)
         }
+        
         appleLoginButton.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview().inset(16)
             $0.top.equalTo(logoImageView.snp.bottom).offset(10)
+            $0.height.equalTo(48)
+        }
+        
+        appleLogo.snp.makeConstraints {
+            $0.height.equalTo(24)
+        }
+        
+        appleLoginStackView.snp.makeConstraints {
+            $0.centerY.centerX.equalTo(appleLoginButton)
+        }
+        
+        appleLoginView.snp.makeConstraints {
+            $0.edges.equalTo(appleLoginStackView)
         }
     }
 }
@@ -85,12 +144,9 @@ extension InitialViewController: ASAuthorizationControllerDelegate, ASAuthorizat
                 guard let accessToken = appleLoginData?.data?.accessToken else { return }
                 print("accessToken:\(accessToken)")
 //                UserDefaults.standard.set("\(accessToken)", forKey: "hasToken")
+                self.presentMainView()
             }
         }
-        guard let mainViewController = UIStoryboard(name: Constant.Storyboard.Main, bundle: nil).instantiateViewController(withIdentifier: "MainNavigationController") as? MainNavigationController else { return }
-        mainViewController.modalPresentationStyle = .fullScreen
-        mainViewController.modalTransitionStyle = .crossDissolve
-        present(mainViewController, animated: true)
     }
     
     // Apple ID 연동 실패
