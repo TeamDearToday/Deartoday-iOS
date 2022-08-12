d5d9813b02d03a700b13b56a1335ae2d771b1b0d
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-08-12 13:43:58 +0900

    [#142] 메시지 확인 엠티뷰 UI 구현 코드로 변경

---
+++ Deartoday/Deartoday/Screen/CheckMessage/Controller/CheckMessagesViewController.swift
@@ -0,0 +1,184 @@
+//
+//  CheckMessagesViewController.swift
+//  Deartoday
+//
+//  Created by 이경민 on 2022/08/11.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class CheckMessagesViewController: UIViewController {
+    
+    // MARK: - Property
+    // MARK: - UI Property
+    
+    private let navigationView = UIView().then {
+        $0.backgroundColor = .clear
+    }
+    
+    private let backButton = UIButton().then {
+        $0.setImage(Constant.Image.icBack, for: .normal)
+        $0.addTarget(self, action: #selector(backButtonDidTap), for: .touchUpInside)
+    }
+    
+    private let messageImageView = UIImageView().then {
+        $0.image = Constant.Image.imgMemoBundle
+    }
+    
+    private let titleLabel = UILabel().then {
+        $0.text = "나의 메시지"
+        $0.font = .h1
+        $0.textColor = .darkGray01
+        $0.setPartialLabelColor(targetStringList: ["메시지"], color: .blue02)
+    }
+    
+    private let descriptionLabel = UILabel().then {
+        $0.text = "미래의 나로부터 도착한 메시지를 확인해보세요"
+        $0.font = .caption2
+        $0.textColor = .gray00
+    }
+    
+    private let emptyView = UIView().then {
+        $0.isHidden = false
+        $0.backgroundColor = .clear
+    }
+    
+    private let emptyMessageImageView = UIImageView().then {
+        $0.image = Constant.Image.imgMemoempty
+    }
+    
+    private let emptyDescriptionLabel = UILabel().then {
+        $0.text = "아직 나에게 도착한 메시지가 없어요!\n지금 바로 시간 여행을 떠나볼까요?"
+        $0.numberOfLines = 0
+        $0.font = .caption2
+        $0.textColor = .gray01
+        $0.setTextWithLineHeight(text: $0.text, lineHeight: 22)
+    }
+    
+    private let timeTravelButton = DDSButton().then {
+        $0.text = "시간 여행 떠나기"
+        $0.hasLeftIcon = true
+        $0.style = .present
+    }
+    
+    private let timeTravelView = UIView().then {
+        $0.backgroundColor = .clear
+        $0.isUserInteractionEnabled = true
+    }
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+        setGesture()
+    }
+    
+    // MARK: - @objc
+    
+    @objc private func backButtonDidTap() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @objc private func timeTravelButtonDidTap() {
+        let timeTravel = TimeTravelViewController()
+        timeTravel.modalTransitionStyle = .crossDissolve
+        timeTravel.modalPresentationStyle = .fullScreen
+        present(timeTravel, animated: true) {
+            self.navigationController?.popViewController(animated: false)
+        }
+    }
+    
+    // MARK: - Custom Method
+    
+    private func setUI() {
+        view.backgroundColor = .white
+    }
+    
+    private func setLayout() {
+        setHierarchy()
+        setConstraint()
+    }
+    
+    private func setHierarchy() {
+        view.addSubviews([navigationView, emptyView])
+        navigationView.addSubviews([backButton, messageImageView,
+                                   titleLabel, descriptionLabel])
+        emptyView.addSubviews([emptyMessageImageView, emptyDescriptionLabel, timeTravelButton])
+        timeTravelButton.addSubview(timeTravelView)
+    }
+    
+    private func setConstraint() {
+        setNavigationBarConstraint()
+        setEmptyViewContraint()
+    }
+    
+    private func setGesture() {
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(timeTravelButtonDidTap))
+        timeTravelView.addGestureRecognizer(gesture)
+    }
+}
+
+// MARK: - Constraints
+
+extension CheckMessagesViewController {
+    private func setNavigationBarConstraint() {
+        navigationView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(0)
+            make.height.equalTo(161)
+        }
+        
+        backButton.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(14)
+            make.leading.equalToSuperview().inset(6)
+            make.width.height.equalTo(44)
+        }
+        
+        messageImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(2)
+            make.trailing.equalToSuperview().inset(16)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(72)
+            make.leading.equalToSuperview().inset(20)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(4)
+            make.leading.equalToSuperview().inset(20)
+        }
+    }
+    
+    private func setEmptyViewContraint() {
+        emptyView.snp.makeConstraints { make in
+            make.top.equalTo(navigationView.snp.bottom).offset(0)
+            make.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide).inset(0)
+        }
+        
+        emptyMessageImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(constraintByNotch(83, 50))
+            make.centerX.equalToSuperview()
+        }
+        
+        emptyDescriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(emptyMessageImageView.snp.bottom).offset(8)
+            make.centerX.equalToSuperview()
+        }
+        
+        timeTravelButton.snp.makeConstraints { make in
+            make.top.equalTo(emptyDescriptionLabel.snp.bottom).offset(12)
+            make.width.equalTo(190)
+            make.height.equalTo(68)
+            make.centerX.equalToSuperview()
+        }
+        
+        timeTravelView.snp.makeConstraints { make in
+            make.top.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+}
