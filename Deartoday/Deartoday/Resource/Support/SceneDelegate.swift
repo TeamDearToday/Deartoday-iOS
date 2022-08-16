06c91d0442a68689afd5bbf1a8c2ac29b68799b1
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-08-17 01:04:41 +0900

    [#142] 파일 원상태로 돌리기

--- Deartoday/Deartoday/Resource/Support/SceneDelegate.swift
+++ Deartoday/Deartoday/Resource/Support/SceneDelegate.swift
@@ -13,12 +13,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-//        guard let scene = (scene as? UIWindowScene) else { return }
-//        self.window = UIWindow(windowScene: scene)
-//        self.window?.rootViewController = SplashViewController()
-//        self.window?.makeKeyAndVisible()
-    
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: scene)
+        self.window?.rootViewController = SplashViewController()
+        self.window?.makeKeyAndVisible()
+        
+//        guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
